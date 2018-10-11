98b9468f1548745ca58b1d289934002abade5f00
Jake <jmvsilfhout@gmail.com>
2018-10-11 13:40:10 +1300

    Template created

---
+++ 5202Assignment/5202Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Name: Add your name here.
+     Id: Add your id number here. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Remember to comment your code.
+namespace _5202Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add your code...
+
+
+
+            //Display closing instructions.
+            Console.WriteLine("Please press ENTER to exit the application.");
+            Console.ReadLine();
+        }
+    }
+}
